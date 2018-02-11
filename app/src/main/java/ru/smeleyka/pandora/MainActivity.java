99c3e71bb68dd7b4b3c9751dd3bba7a4416a7c35
smeleyka <smeleyka@gmail.com>
2018-02-11 22:42:32 +0300

    dialog add

--- app/src/main/java/ru/smeleyka/pandora/MainActivity.java
+++ app/src/main/java/ru/smeleyka/pandora/MainActivity.java
@@ -1,15 +1,17 @@
 package ru.smeleyka.pandora;
 
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private static SharedPreferences sharedPref;
+    AlertDialog.Builder ad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,16 +19,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         sharedPref = getPreferences(MODE_PRIVATE);
 
+        initDialog();
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                ad.show();
+            }
+        });
+    }
+
+    private void initDialog() {
+        ad = new AlertDialog.Builder(this);
+        ad.setView(R.layout.dialog_addbutton);
+        ad.setPositiveButton("ОК", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                System.out.println("OK");
+            }
+        });
+        ad.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                System.out.println("NO");
             }
         });
     }
