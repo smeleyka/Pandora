86d7c7f691326aaf11006228d2a2ff985048f402
smeleyka <smeleyka@gmail.com>
2018-02-13 00:34:06 +0300

    permissions

--- app/src/main/java/ru/smeleyka/pandora/MainActivity.java
+++ app/src/main/java/ru/smeleyka/pandora/MainActivity.java
@@ -2,7 +2,6 @@ package ru.smeleyka.pandora;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -10,7 +9,6 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
 import android.support.constraint.ConstraintSet;
 import android.support.design.widget.FloatingActionButton;
@@ -19,23 +17,21 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.Layout;
-import android.util.ArraySet;
-import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.google.gson.Gson;
+
+import java.io.Serializable;
+import java.lang.reflect.Array;
 import java.sql.SQLOutput;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 1;
@@ -44,7 +40,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button button;
     private AlertDialog.Builder ad;
     private View contentLayout;
-    private HashMap map;
+    private String[] arr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +52,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         initDialog();
         initButton();
         permissonRequest();
+
     }
 
 //    @Override
@@ -71,7 +68,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onStart() {
 
+        ArrayList<Button> buttons  = new ArrayList<>();
+        for (int i = 0; i <5 ; i++) {
+            buttons.add(new Button(this));
+            buttons.get(i).setText("Button NO "+i);
+        }
         loadSharedPreferences();
+        System.out.println(Arrays.toString(arr));
 
 
         super.onStart();
@@ -80,18 +83,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void loadSharedPreferences() {
         SharedPreferences loadSP = getPreferences(MODE_PRIVATE);
         String prefValue = loadSP.getString("button1", "DEFAULT");
+        arr = loadSP.getStringSet("buttons",new HashSet<String>()).toArray(new String[2]);
         System.out.println(prefValue);
         System.out.println("TEST");
         System.out.println(loadSP.getAll().toString());
+        System.out.println("ARRAY");
+        System.out.println(Arrays.toString(arr));
     }
 
     private void createSharedPreferences() {
         sharedPref = getPreferences(MODE_PRIVATE);
         SharedPreferences.Editor sharedPrefEdit = sharedPref.edit();
+
+        Set<String> arrSet = new HashSet<>();
+        arrSet.add("TEST1");
+        arrSet.add("TEST2");
+        arrSet.add("TEST3");
+        arrSet.add("TEST4");
+
+
+
         sharedPrefEdit.putString("button1", "test1");
         sharedPrefEdit.putString("button2", "test2");
         sharedPrefEdit.putString("button3", "test3");
         sharedPrefEdit.putString("button4", "test4");
+
+        sharedPrefEdit.putStringSet("buttons",arrSet);
+
         sharedPrefEdit.commit();
     }
 
